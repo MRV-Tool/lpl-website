84f63c763c44a19d52fd9c98064e2a0956b220d7
igeek-apeksha <apeksha@igeektech.email>
2024-03-20 18:30:04 +0530

    Changed Config File

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,1 @@
-// export const BASE_URL = 'https://mrv-be-d5fd6e4877a4.herokuapp.com';
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const BASE_URL = 'https://mrv-be-d5fd6e4877a4.herokuapp.com';
